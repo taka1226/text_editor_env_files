bfc682d20b21193723810615b79690405fed53f9
taka1226 <57122641+taka1226@users.noreply.github.com>
2021-04-08 10:16:07 +0900

    Create emacs.el

---
+++ emacs.el
@@ -0,0 +1,127 @@
+(require 'package)
+(add-to-list 'package-archives'("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; 環境を日本語、UTF-8にする
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; テーマを黒にする
+(load-theme 'tango-dark t)
+
+;; 画面の最大化
+(toggle-frame-maximized)
+
+;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+
+;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
+
+;; 括弧を自動で補完する
+(electric-pair-mode 1)
+
+;; tabにスペース４つを利用
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; デフォルトの起動時のメッセージを表示しない
+(setq inhibit-startup-message t)
+
+;; 列の番号
+(column-number-mode t)
+
+;; 行番号の表示
+(global-linum-mode t)
+
+;; 1行ごとの改ページ
+(setq scroll-conservatively 1)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; メニューバーの非表示
+(menu-bar-mode -1)
+
+;; ツールバーの非表示
+(tool-bar-mode -1)
+
+;; リージョンのハイライト
+(transient-mark-mode 1)
+
+;; タイトルにフルパス表示
+(setq frame-title-format "%f")
+
+;;current directory 表示
+(let ((ls (member 'mode-line-buffer-identification
+                  mode-line-format)))
+  (setcdr ls
+    (cons '(:eval (concat " ("
+            (abbreviate-file-name default-directory)
+            ")"))
+            (cdr ls))))
+
+;; 時刻をモードラインに表示
+(display-time-mode t)
+
+;; 別のウィンドウ
+(global-set-key "\C-t" 'other-window)
+
+;; neotree（サイドバー）
+(use-package neotree
+  :init
+  (setq-default neo-keymap-style 'concise)
+  :config
+  (setq neo-smart-open t)
+  (setq neo-create-file-auto-open t)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (bind-key [f8] 'neotree-toggle)
+  (bind-key "RET" 'neotree-enter-hide neotree-mode-map)
+  (bind-key "a" 'neotree-hidden-file-toggle neotree-mode-map)
+  (bind-key "<left>" 'neotree-select-up-node neotree-mode-map)
+  (bind-key "<right>" 'neotree-change-root neotree-mode-map))
+
+
+;; Change neotree's font size
+;; Tips from https://github.com/jaypei/emacs-neotree/issues/218
+(defun neotree-text-scale ()
+  "Text scale for neotree."
+  (interactive)
+  (text-scale-adjust 0)
+  (text-scale-decrease 1)
+  (message nil))
+(add-hook 'neo-after-create-hook
+      (lambda (_)
+        (call-interactively 'neotree-text-scale)))
+
+;; neotree enter hide
+;; Tips from https://github.com/jaypei/emacs-neotree/issues/77
+(defun neo-open-file-hide (full-path &optional arg)
+  "Open file and hiding neotree.
+The description of FULL-PATH & ARG is in `neotree-enter'."
+  (neo-global--select-mru-window arg)
+  (find-file full-path)
+  (neotree-hide))
+
+(defun neotree-enter-hide (&optional arg)
+  "Neo-open-file-hide if file, Neo-open-dir if dir.
+The description of ARG is in `neo-buffer--execute'."
+  (interactive "P")
+  (neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package all-the-icons twig-mode php-mode neotree)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
